a93de2525409fbdfab4d660829da4145bc03e417
kaesoftware@gmail.com <kaesoftware@gmail.com>
2025-03-12 22:23:50 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
+const https = require('https');
+const fs = require('fs');
+const app = express();
+const http = require('http').Server(app);
+
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+//app.set('view engine','html');
+
+app.use(express.static('public', express.static(path.join(__dirname, 'public'))));
+
+app.get('/', (req, res) => {
+  res.render("index.ejs");
+});
+
+
+const port = 3000;
+const ipAddress = '127.0.0.1'; // Örnek bir IP adresi, kendi IP'nizi kullanmalısınız
+
+app.set('host', '127.0.0.1');
+
+http.listen(3000, ipAddress, () => {
+  console.log(`Sunucu çalışıyor, IP: ${ipAddress}, Port: ${port}`);
+});
